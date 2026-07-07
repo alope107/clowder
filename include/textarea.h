79bb29689aad0d9db0c5a878eaf9d854defc5062
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-07-07 16:45:06 +0000

    WIP textarea

--- include/textarea.h
+++ include/textarea.h
@@ -2,13 +2,34 @@
 
 #include <bn_fixed_point.h>
 #include <bn_sprite_ptr.h>
+#include <bn_sprite_text_generator.h>
+#include <bn_string.h>
 #include <bn_vector.h>
 
 #include "limits.h"
 
+static constexpr int MAX_TEXTAREA_LINES = 2;
+static constexpr int MAX_TEXTAREA_CHARS = MAX_LINE_LEN*MAX_TEXTAREA_LINES;
+static constexpr int LINE_HEIGHT = 18;
+
+
 class textarea {
     public:
-        textarea(bn::fixed_point start, bn::fixed max_x);
+        textarea(bn::sprite_text_generator& gen, 
+                 bn::fixed_point start,
+                 bn::fixed max_x);
+        void clear();
+        bool fits(bn::string_view word);
+        void add_word(bn::string_view word);
     private:
-        bn::vector<bn::sprite_ptr, MAX_LINE_LEN*2> _sprs; // sprites for the text, should be far more than enough
+        bool _fits_on_line(bn::string_view word);
+
+        bn::sprite_text_generator& _gen;
+        bn::vector<bn::string<MAX_LINE_LEN>, MAX_TEXTAREA_LINES> _lines;
+        bn::vector<bn::sprite_ptr, MAX_TEXTAREA_CHARS> _sprs; // sprites for the text, should be far more than enough
+        bn::fixed_point _start;
+        bn::fixed _max_line_width;
+
+        int _line; // index of line currently being edited
+        int _line_width; // width (in pixels) of the line currently being edited
 };
---
+++ src/textarea.cpp
@@ -0,0 +1,64 @@
+// #include "textarea.h"
+
+// // how many pixels taken up by a space in text
+// static constexpr int SPACE_WIDTH = 16;
+
+// textarea::textarea(bn::sprite_text_generator& gen, 
+//                  bn::fixed_point start,
+//                  bn::fixed max_x) :
+//                 _gen(gen),
+//                 _text(""),
+//                 _sprs({}),
+//                 _start(start),
+//                 _max_line_width(max_x - start.x()),
+//                 _line(0){
+// }
+
+// void textarea::clear() {
+//     _text = "";
+//     _sprs.clear();
+//     _line = 0;
+// }
+
+// int string_width(bn::string_view word, const bn::sprite_font & font) {
+//     auto widths = font.character_widths_ref();
+//     constexpr char STARTING_CHAR = 32;
+//     int total_width = 0;
+//     for (const char c : word) {
+//         total_width += widths[c - STARTING_CHAR];
+//     }
+//     return total_width;
+// }
+
+// void textarea::add_word(bn::string_view word) {
+//     // TODO: Optimize repeated fit checks?
+//     BN_ASSERT(fits(word), "Word does not fit in textarea");
+
+
+//     if(!_fits_on_line(word)) {
+//         _line++;
+//         _line_width = 0;
+//         _lines[_line] = "";
+//     } else {
+//         _lines[_line] += " ";
+//         _line_width += SPACE_WIDTH;
+//     }
+
+//     _line_width += string_width(word, _gen.font());
+//     _lines[_line] += word;
+
+//     _sprs.clear();
+//     for(int i = 0; i< _lines.size(); i++) {
+//         bn::fixed y = _start.y() + i*LINE_HEIGHT;
+//     }
+// }
+
+// // makes simplifying assumption that no single word will be wider than the full line width
+// bool textarea::fits(bn::string_view word) {
+//     return _fits_on_line(word) || _line < (MAX_TEXTAREA_LINES - 1);
+// }
+
+// bool textarea::_fits_on_line(bn::string_view word) {
+//     int requested_width = string_width(word, _gen.font()) + SPACE_WIDTH;
+//     return _line_width + requested_width < _max_line_width;
+// }
