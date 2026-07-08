97fbfa7ced21287a1878054f9c17289302b37c1e
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-07-08 14:45:49 +0000

    preprocess beowulf text

---
+++ preprocess/split_text.py
@@ -0,0 +1,28 @@
+import re
+import sys
+
+# /**
+#  * ash = |
+#  * eth = ~
+#  * thorn = }
+#  */
+
+with open(sys.argv[1]) as f:
+    text = f.read()
+    trans = text.translate(str.maketrans(
+        {
+            # "\n": " ",
+            # "\r": None,
+            "æ": "|",
+            "Æ": "|",
+            "ð": "~",
+            "Ð": "~",
+            "þ" :"}",
+            "Þ" :"}",
+        }
+    ))
+    trans = trans.replace("\n", " ")
+    trans = re.sub(r"(\S+) ", r'"\1", ', text)
+
+with open(sys.argv[2], 'w') as f:
+    f.write(trans)
